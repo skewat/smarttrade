7ae5b57e446d6ea19c535585009a63d842061832
anupkewat <anup.kewat@gmail.com>
2025-03-24 18:04:11 +0530

    Adding CSV  nifty_spot.csv

---
+++ symboltoken.py
@@ -0,0 +1,11 @@
+def get_symbol_token(trading_symbol, exchange="NFO"):
+    url = "https://apiconnect.angelbroking.com/rest/secure/angelbroking/order/v1/searchScrip"
+    payload = {"exchange": exchange, "searchscrip": trading_symbol}
+    response = requests.post(url, json=payload, headers=headers)
+    #print(response.text)
+    data = response.json()
+    
+    if data.get("status") and "data" in data and len(data["data"]) > 0:
+        return data["data"][0]["symboltoken"]
+    print(f"[❌] Symbol token not found for {trading_symbol}")
+    return None
