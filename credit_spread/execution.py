db4c9ea522e382f33a8a8d47c78c1e7125f7b8ea
Chandrakant Kewat <ckewat@ubuntu-cloudpanel.localhost>
2025-07-06 21:24:15 +0530

    Added indicator in CSV

--- credit_spread/execution.py
+++ credit_spread/execution.py
@@ -37,6 +37,7 @@ def run_live(connector, df):
     df['atr_lower'] = df['close_daily'] - df['ATR']
 
     df = indicators.add_ema_crossover(df, 'ema_fast', 'ema_slow')
+    df.to_csv('atr_ema_indicator.csv', index=False)
 
     current_position = None
     previous_day_trend = None
